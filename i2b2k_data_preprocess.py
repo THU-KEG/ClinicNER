f8d82248a6b7f2d66e20f34d7cd5e3cb68475215
YanZhangN <yanzhang0110@gmail.com>
2017-07-06 12:05:57 +0800

    word2vec

--- i2b2k_data_preprocess.py
+++ i2b2k_data_preprocess.py
@@ -1,5 +1,7 @@
 import os
 import re
+import nltk
+import sys
 base_dir = 'i2b2k/2010'
 train_dir = os.path.join(base_dir,'concept_assertion_relation_training_data/beth/')
 train_labeldir = os.path.join(train_dir,'concept')
@@ -10,6 +12,7 @@ test_txtdir = os.path.join(base_dir,'test_data/')
 
 train_outputpath = os.path.join(base_dir,'train.txt')
 test_outputpath = os.path.join(base_dir,'test.txt')
+category_outputpath = os.path.join(base_dir,'category.txt')
 
 category2mentions = {}
 error = 0
@@ -31,7 +34,10 @@ def rawdata2bio(text_dir, label_dir, outputpath):
 	out = open(outputpath,'w')
 	out.writelines(writebuf)
 def naiveTokenize(sent):
-	return [[i,'O'] for i in re.split('\s+', sent)]
+	tokens = sent.split()
+	tagged = nltk.pos_tag(tokens)
+	#print tagged
+	return [[i[0],i[1],'O'] for i in tagged]
 
 def generateBIO(textlines,labellines,writebuf,tokenizer = naiveTokenize):
 	global error
@@ -56,28 +62,44 @@ def generateBIO(textlines,labellines,writebuf,tokenizer = naiveTokenize):
 		tokens_mention = ' '.join([tokens[lineno][i][0] for i in range(bindex,eindex+1)]).lower()
 		#print tokens[lineno][bindex],mention,tokens_mention
 		if tokens_mention !=mention:
-			'%s not equal %s'%(tokens_mention,mention)
+			print '%s not equal %s'%(tokens_mention,mention)
 			error+=1
+			#sys.exit(-1)
 		###modify BIO
 		for i in range(bindex,eindex+1):
-			if tokens[lineno][i][1]!= 'O':
+			if tokens[lineno][i][2]!= 'O':
 				break
-			tokens[lineno][i][1] = 'B-'+category.upper() if i == bindex else 'I-'+category.upper()
+			tokens[lineno][i][2] = 'B-'+category.upper() if i == bindex else 'I-'+category.upper()
 	for line in tokens:
 		###filt all 'O' sentences
 		allO = True
 		for token in line:
-			if token[1] != 'O':
+			if token[2] != 'O':
 				allO = False
 				break
 		if not allO:
-			writebuf.extend(t[0]+'\t'+t[1]+'\n' for t in line)
+			writebuf.extend(t[0]+'\t'+t[1]+'\t'+t[2]+'\n' for t in line)
 			writebuf.append('\n')
 
-#print naiveTokenize('abc ddd   qowe')
-#rawdata2bio(train_txtdir,train_labeldir, train_outputpath)
-rawdata2bio(test_txtdir,test_labeldir, test_outputpath)
-print category2mentions,len(category2mentions)
-print category2mentions.keys()
-print 'error number:%s',error
+
+def main():
+	rawdata2bio(train_txtdir,train_labeldir, train_outputpath)
+	rawdata2bio(test_txtdir,test_labeldir, test_outputpath)
+
+
+	train_dir = os.path.join(base_dir,'concept_assertion_relation_training_data/partners/')
+	train_labeldir = os.path.join(train_dir,'concept')
+	train_txtdir = os.path.join(train_dir,'txt')
+	rawdata2bio(train_txtdir,train_labeldir, train_outputpath+'.partners.txt')
+
+	#print category2mentions,len(category2mentions)
+	#print category2mentions.keys()
+	print 'error number:%s'%error
+
+	out = open(category_outputpath,'w')
+	for key in category2mentions:
+		out.write(key+'\t'+':::'.join([i for i in category2mentions[key]])+'\n')
+
+if __name__=='__main__':
+	main()
 
--- nertest.py
+++ nertest.py
@@ -4,52 +4,59 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelBinarizer
 import sklearn
 import pycrfsuite
+import i2b2k_data
 
 print(sklearn.__version__)
-print nltk.corpus.conll2002.fileids()
-train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
-test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+#print nltk.corpus.conll2002.fileids()
+#train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+train_sents = i2b2k_data.iob_sents('train')
+#test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+test_sents = i2b2k_data.iob_sents('test')
 print train_sents[0]
 
 def word2features(sent, i):
     word = sent[i][0]
     postag = sent[i][1]
-    features = [
-        'bias',
-        'word.lower=' + word.lower(),
-        'word[-3:]=' + word[-3:],
-        'word[-2:]=' + word[-2:],
-        'word.isupper=%s' % word.isupper(),
-        'word.istitle=%s' % word.istitle(),
-        'word.isdigit=%s' % word.isdigit(),
-        'postag=' + postag,
-        'postag[:2]=' + postag[:2],
-    ]
+    features = {
+        'bias':1.0,
+        'word.lower=' + word.lower():1.0,
+        'word[-3:]=' + word[-3:]:1.0,
+        'word[-2:]=' + word[-2:]:1.0,
+        'word.isupper=%s' % word.isupper():1.0,
+        'word.istitle=%s' % word.istitle():1.0,
+        'word.isdigit=%s' % word.isdigit():1.0,
+        'postag=' + postag:1.0,
+        'postag[:2]=' + postag[:2]:1.0
+    }
     if i > 0:
         word1 = sent[i-1][0]
         postag1 = sent[i-1][1]
-        features.extend([
-            '-1:word.lower=' + word1.lower(),
-            '-1:word.istitle=%s' % word1.istitle(),
-            '-1:word.isupper=%s' % word1.isupper(),
-            '-1:postag=' + postag1,
-            '-1:postag[:2]=' + postag1[:2],
-        ])
+        newfeatures = {
+            '-1:word.lower=' + word1.lower():1.0,
+            '-1:word.istitle=%s' % word1.istitle():1.0,
+            '-1:word.isupper=%s' % word1.isupper():1.0,
+            '-10:word.lower=' + word1.lower()+'_'+word.lower():1.0,
+            '-1:postag=' + postag1:1.0,
+            '-1:postag[:2]=' + postag1[:2]:1.0
+        }
+        features = dict(features,**newfeatures)
     else:
-        features.append('BOS')
+        features['BOS'] = 1.0
         
     if i < len(sent)-1:
         word1 = sent[i+1][0]
         postag1 = sent[i+1][1]
-        features.extend([
-            '+1:word.lower=' + word1.lower(),
-            '+1:word.istitle=%s' % word1.istitle(),
-            '+1:word.isupper=%s' % word1.isupper(),
-            '+1:postag=' + postag1,
-            '+1:postag[:2]=' + postag1[:2],
-        ])
+        newfeatures = {
+            '+1:word.lower=' + word1.lower():1.0,
+            '+1:word.istitle=%s' % word1.istitle():1.0,
+            '+1:word.isupper=%s' % word1.isupper():1.0,
+            '01:word.lower=' +word.lower()+'_'+word1.lower():1.0,
+            '+1:postag=' + postag1:1.0,
+            '+1:postag[:2]=' + postag1[:2]:1.0
+        }
+        features = dict(features,**newfeatures)
     else:
-        features.append('EOS')
+        features['EOS'] = 1.0
                 
     return features
 
@@ -80,9 +87,45 @@ for xseq, yseq in zip(X_train, y_train):
 trainer.set_params({
     'c1': 1.0,   # coefficient for L1 penalty
     'c2': 1e-3,  # coefficient for L2 penalty
-    'max_iterations': 50,  # stop earlier
+    'max_iterations': 100,  # stop earlier
 
     # include transitions that are possible, but not observed
     'feature.possible_transitions': True
 })
-trainer.train('conll2002-esp.crfsuite')
+trainer.train('i2b2k2010-esp.crfsuite')
+
+
+
+##############################test################
+tagger = pycrfsuite.Tagger()
+tagger.open('i2b2k2010-esp.crfsuite')
+example_sent = test_sents[0]
+print ' '.join(sent2tokens(example_sent))+'\n\n'
+
+print "Predicted:", ' '.join(tagger.tag(sent2features(example_sent)))
+print "Correct:  ", ' '.join(sent2labels(example_sent))
+
+def bio_classification_report(y_true, y_pred):
+    """
+    Classification report for a list of BIO-encoded sequences.
+    It computes token-level metrics and discards "O" labels.
+    
+    Note that it requires scikit-learn 0.15+ (or a version from github master)
+    to calculate averages properly!
+    """
+    lb = LabelBinarizer()
+    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+        
+    tagset = set(lb.classes_) - {'O'}
+    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+    
+    return classification_report(
+        y_true_combined,
+        y_pred_combined,
+        labels = [class_indices[cls] for cls in tagset],
+        target_names = tagset,
+    )
+y_pred = [tagger.tag(xseq) for xseq in X_test]
+print(bio_classification_report(y_test, y_pred))
